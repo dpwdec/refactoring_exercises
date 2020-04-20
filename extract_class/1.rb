a40e5b50c3e4ef272794a8083757aba9f4ac13d3
Declan <declankolakowski@gmail.com>
2020-04-20 11:20:10 +0000

    Completed extract class exetcise 1

--- extract_class/1.rb
+++ extract_class/1.rb
@@ -1,13 +1,17 @@
 # Understands sanitising a user's email and sending them an email
 class UserContacter
+  def initialize
+    @santiser = EmailSanitiser.new
+  end
+  
   def send_email_to(user)
-    mail = Mail.new(sanitised_email_for(user))
+    mail = Mail.new(@santiser.sanitised_email_for(user))
     
     mail.send_message
   end
+end
 
-  private
-
+class EmailSanitiser
   def sanitised_email_for(user)
     email.strip
   end
