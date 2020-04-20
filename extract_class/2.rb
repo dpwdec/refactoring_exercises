e863ce7be0dce8f863e34be9c28f7efb7185bf04
Declan <declankolakowski@gmail.com>
2020-04-20 11:30:21 +0000

    Extracted class from exercise 2

--- extract_class/2.rb
+++ extract_class/2.rb
@@ -1,14 +1,19 @@
 # Understands detecting the browser type and sending the user a message
 class BrowserMessage
-  def tell_browser_type(browser)
-    safari_message = "You are using the Safari browser."
-    not_safari_message = "You are not using a Safari browser."
-
-    safari?(browser) ? puts safari_message : puts not_safari_message
+  
+  SAFARI_MESSAGE = "You are using the Safari browser."
+  NOT_SAFARI_MESSAGE = "You are not using a Safari browser."
+  
+  def initialize
+    @browser_checker = BrowserChecker.new
   end
+  
+  def tell_browser_type(browser)
+    puts @browser_checker.safari?(browser) ? SAFARI_MESSAGE : NOT_SAFARI_MESSAGE
+  end
+end
 
-  private
-
+class BrowserChecker
   def safari?(browser)
     browser.type == "Safari"
   end
