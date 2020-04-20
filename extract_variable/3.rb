b079450a3cb227f7a92d6a0b44a3e45b33ffd1a4
Declan <declankolakowski@gmail.com>
2020-04-20 11:08:17 +0000

    completed exercise 3

--- extract_variable/3.rb
+++ extract_variable/3.rb
@@ -1,7 +1,12 @@
 def send_correct_page(browser, window)
+  
+  message = ""
+  
   if(browser.type == "Safari" && window.size < "768px")
-    return "You are using the Safari browser in a small window."
+    message = "You are using the Safari browser in a small window."
   else
-    return "You are not using the Safari browser or have a big window."
+    message = "You are not using the Safari browser or have a big window."
   end
+  
+  return message
 end
