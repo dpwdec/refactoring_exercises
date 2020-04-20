5fb4ebc0cbad73d81e3592226684870f70204b9b
Declan <declankolakowski@gmail.com>
2020-04-20 11:11:39 +0000

    Completed exercise 4

--- extract_variable/4.rb
+++ extract_variable/4.rb
@@ -1,14 +1,11 @@
 class Mail
   def initialize(email, fancy = false)
     @email = email
-    @fancy = fancy
+    @message = "Welcome to MyProduct"
+    @message += ", fancy person!" if fancy
   end
 
   def send_message
-    if fancy
-      MailServer.connect("http://mixmax.com", api_key: "f20506xx808c").send_message({ to: @email, body: "Welcome to MyProduct, fancy person!" })
-    else
-      MailServer.connect("http://mixmax.com", api_key: "f20506xx808c").send_message({ to: @email, body: "Welcome to MyProduct" })
-    end
+      MailServer.connect("http://mixmax.com", api_key: "f20506xx808c").send_message({ to: @email, body: @message })
   end
 end
