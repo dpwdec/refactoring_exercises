0061cf744f53f9ba70117faf6e29b4897526b3a8
Declan <declankolakowski@gmail.com>
2020-04-20 11:04:40 +0000

    Refactored exercise 2

--- extract_variable/2.rb
+++ extract_variable/2.rb
@@ -1,7 +1,6 @@
 def tell_browser_type(browser)
-  if(browser.type == "Safari")
-    puts "You are using the Safari browser."
-  else
-    puts "You are using a non-Safari browser."
-  end
+  
+  message = browser.type == "Safari" ? "You are using the Safari browser." : "You are using a non-Safari browser."
+  
+  puts message
 end
