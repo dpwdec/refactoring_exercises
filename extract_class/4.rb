f554018f7670fde461b7177284333d065f429134
dpwdec <declankolakowski@gmail.com>
2020-04-21 09:04:00 +0000

    Completed class extraction for exercise 4

--- extract_class/4.rb
+++ extract_class/4.rb
@@ -2,17 +2,21 @@
 class Mail
   def initialize(email, fancy = false)
     @email = email
-    @fancy = fancy
+    @body = Body.new(fancy)
   end
 
   def send_message
     mail_server = MailServer.connect("http://mixmax.com", api_key: "f20506xx808c")
-    mail_server.send_message({ to: @email, body: body })
+    mail_server.send_message({ to: @email, body: @body.generate })
   end
+end
 
-  private
-
-  def body
+class Body
+  def initialize(fancy = false)
+    @fancy = fancy
+  end
+  
+  def generate
     base_body = "Welcome to MyProduct"
     @fancy ? "#{ base_body }, fancy person!" : base_body
   end
